ea2309f5b2ffe237506be4a8bc2e86f05850748c
Yuri Bounich <b52ybunich@gmail.com>
2024-11-13 11:17:46 +0200

    struktura

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,67 +1,27 @@
-import contactsData from '../assets/contacts.json';
-import ContactForm from './ContactForm/ContactForm';
+// import contactsData from '../assets/contacts.json';
 import '../index.css';
-import SearchBox from './SearchBox/SearchBox';
-import ContactList from './ContactList/ContactList';
-import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
+// import { useState, useEffect } from 'react';
+// import { nanoid } from 'nanoid';
+import Work from './Work/Work';
+import SearchBar from './SearchBar/SearchBar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import ImageCard from './ImageCard/ImageCard';
+import Loader from './Loader/Loader';
+import ErrorMessage from './ErrorMessage/ErrorMessage';
+import LoadMoreBtn from './LoadMoreBtn/LoadMoreBtn';
+import ImageModal from './ImageModal/ImageModal';
 
 const App = () => {
-  // Ініціалізація стану з локального сховища або початкових значень
-  const [values, setValues] = useState('');
-  const [contacts, setContacts] = useState(
-    () => JSON.parse(localStorage.getItem('contacts')) || contactsData
-  );
-  // Ініціалізація зі сховища або даних за замовчуванням
-
-  useEffect(() => {
-    // Зберігаємо дані в локальному сховищі щоразу при зміні `contacts`
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  // Функція для обробки змін у фільтрі
-  const handleFilterChange = event => {
-    setValues(event.target.value); // Оновлює значення фільтру
-  };
-
-  // Фільтрування контактів на основі фільтру
-  const filteredContacts = contacts.filter(
-    contact =>
-      contact.name &&
-      contact.name.toLowerCase().includes(values ? values.toLowerCase() : '')
-  );
-
-  const handleDeleteTodo = id => {
-    // console.log(id);
-    // const newContacts = contacts.filter(item => item.id != id);
-    // //використовуємо фільт для відсіювання id які не дорівнюють id що видаляється
-    // console.log(newContacts);
-    setContacts(prev => prev.filter(item => item.id != id));
-    // у setContacts перезаписали відфільтрований масив
-  };
-
-  const handleAddTodo = ({ login, password }) => {
-    const newTodo = {
-      id: nanoid(),
-      name: login,
-      number: password,
-    };
-    setContacts(prev => [...prev, newTodo]);
-  };
-
   return (
     <div>
-      <ContactForm handleAddTodo={handleAddTodo} />
-      <SearchBox
-        values={values}
-        onFilterChange={handleFilterChange}
-        /* Передаємо значення фільтру та функцію для його оновлення */
-      />
-      <ContactList
-        contacts={filteredContacts}
-        /* Передаємо відфільтрований список контактів */
-        handleDeleteTodo={handleDeleteTodo}
-      />
+      <ImageModal />
+      <LoadMoreBtn />
+      <ErrorMessage />
+      <Loader />
+      <ImageCard />
+      <ImageGallery />
+      <SearchBar />
+      <Work />
     </div>
   );
 };
